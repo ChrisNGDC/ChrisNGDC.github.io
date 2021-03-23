d580cda163556e6ded6485eba1fad7f3b860737e
ChrisNGDC <chrisgabor95@gmail.com>
2021-03-23 16:11:49 -0300

    cambios simples

--- docs/resources/js/script.js
+++ docs/resources/js/script.js
@@ -2,3 +2,13 @@ const menuToggle = document.querySelector('.toggle');
 const showcase = document.querySelector('.showcase');
 
 menuToggle.addEventListener('click', () => { menuToggle.classList.toggle('active'); showcase.classList.toggle('active'); })
+
+function submitMessaje() {
+    var name = document.getElementsByName("Nombre").value;
+    var mail = document.getElementsByName("Mail").value;
+    var asunto = document.getElementsByName("Asunto").value;
+    var mensaje = document.getElementsByName("Mensaje").value;
+
+    var file = generateTxtFile(name)
+    file.style.display = 'inline-block';
+}
