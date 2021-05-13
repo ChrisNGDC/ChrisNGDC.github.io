3943eea06a4dec72a951262897b0b6244665daa1
ChrisNGDC <chrisgabor95@gmail.com>
2021-05-13 20:03:19 -0300

    asd

--- docs/src/js/game_bullet.js
+++ docs/src/js/game_bullet.js
@@ -3,7 +3,7 @@ export default class Bullet {
       this.image = document.getElementById("bullet_img");
       this.width = 32;
       this.height = 32;
-      this.max_speed = 128;
+      this.max_speed = 64;
       this.speed = 0;
       this.status = "ready";
       this.position = {
@@ -24,11 +24,12 @@ export default class Bullet {
     update(delta_time) {
         if (!delta_time) return;
         let velocity = this.speed / delta_time;
-        if (this.position.y < 0 || this.status == "ready"){
+        if (this.position.y < 0){
             this.position.y = 0;
-            this.position.x = this.game_max_width + this.width;
+            this.position.x = 0 - this.width;
             this.status = "ready";
-        } else {
+        }
+        if (this.status == "firing"){
             this.position.y -= velocity;
         }
     }
