e17c819b5b3ef4410b06a90478cf71be20ddc01c
ChrisNGDC <chrisgabor95@gmail.com>
2021-05-13 15:13:19 -0300

    Mejora del tiempo

--- docs/resources/js/game_index.js
+++ docs/resources/js/game_index.js
@@ -7,4 +7,19 @@ const game_width = 800;
 const game_height = 600;
 
 let player = new Player(game_width, game_height);
+
+let last_time = 0
+
 player.draw(ctx);
+
+function game_loop(time_stamp) {
+    let delta_time = time_stamp -  last_time;
+    last_time = time_stamp;
+    ctx.clearRec(0, 0, game_width, game_height);
+    player.update(delta_time);
+    player.draw(ctx);
+
+    requestAnimationFrame(game_loop);
+}
+
+game_loop();
--- docs/resources/js/game_player.js
+++ docs/resources/js/game_player.js
@@ -5,11 +5,17 @@ export default class Player {
         this.height = 64;
         this.position = {
             x : game_width / 2 - this.width / 2,
-            y : game_height - 24 - this.height
+            y : game_height - this.height - 24
         };
     }
 
     draw(ctx) {
         ctx.drawImage(this.image, this.position.x, this.position.y);
+    }
+
+    update(delta_time) {
+        if (!delta_time) 
+            return
+        this.position.x += 5 / delta_time;
     }
 }
