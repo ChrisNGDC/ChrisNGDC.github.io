5374aa4e99af0030f4374e220976ec567d943d3a
ChrisNGDC <chrisgabor95@gmail.com>
2021-08-24 10:15:27 -0300

    .

--- docs/src/js/cv.js
+++ docs/src/js/cv.js
@@ -1,12 +1,14 @@
-var elems = document.getElementsByClassName("progressbar");
+const elems = document.getElementsByClassName("progressbar");
 
-for (let i = 0; i < elems.length; i++) {
+for (let i = 0; i < 6; i++) {
     var elem = elems[i].firstChild;
+    console.log(elem);
     var percentage = elem.innerHTML;
+    console.log(percentage);
     var bar = elems[i].lastChild;
+    console.log(bar);
 
-    bar.style.width = percentage + '%';
+    bar.style.width = percentage + "%";
 }
 
 var prueba = document.getElementsByClassName("nombre")[0];
-prueba.innerHTML = "Funciona";
