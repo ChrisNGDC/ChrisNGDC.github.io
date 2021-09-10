7169be375e42b7ff7432c2204512759c16509906
ChrisNGDC <chrisgabor95@gmail.com>
2021-09-09 21:40:17 -0300

    cv

--- docs/src/js/cv.js
+++ docs/src/js/cv.js
@@ -1,14 +1,8 @@
 const elems = document.getElementsByClassName("progressbar");
 
 for (let i = 0; i < 6; i++) {
-    var elem = elems[i].firstChild;
-    console.log(elem);
-    var percentage = elem.innerHTML;
-    console.log(percentage);
-    var bar = elems[i].lastChild;
-    console.log(bar);
-
-    bar.style.width = percentage + "%";
+    var elem = elems[i].getElementsByClassName("value")[0];
+    var percentage = elem.firstChild;
+    var bar = elems[i].getElementsByClassName("bar")[0];
+    bar.style.width = percentage.nodeValue + "%";
 }
-
-var prueba = document.getElementsByClassName("nombre")[0];
