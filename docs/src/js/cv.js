94faf1d69eaf15687edcfa038ebd53c35f08014f
ChrisNGDC <chrisgabor95@gmail.com>
2022-08-07 18:32:40 -0300

    CV actualizado

--- docs/src/js/cv.js
+++ docs/src/js/cv.js
@@ -1,6 +1,6 @@
 const elems = document.getElementsByClassName("progressbar");
 
-for (let i = 0; i < 6; i++) {
+for (let i = 0; i < elems.length; i++) {
     var elem = elems[i].getElementsByClassName("value")[0];
     var percentage = elem.firstChild;
     var bar = elems[i].getElementsByClassName("bar")[0];
