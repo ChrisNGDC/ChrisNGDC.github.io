239038e228f84a64e8ffacffecd8ffaf590a3505
ChrisNGDC <chrisgabor95@gmail.com>
2023-02-28 16:02:35 -0300

    Skills-icons

--- docs/CV-TICMAS/index.js
+++ docs/CV-TICMAS/index.js
@@ -1,6 +1,16 @@
 // Generacion de lista de habilidades
 
-const abilitiesNamesPercentage = [["HTML",90], ["CSS",85], ["JavaScript",95], ["NodeJS",70], ["MySQL",75], ["C#",67], ["C++",82], ["PHP",78], ["Python",93], ["Testing",69]];
+const abilitiesNamesPercentage = [
+                                ["HTML",'<i class="fa-brands fa-html5" title="HTML5"></i>',90],
+                                ["CSS",'<i class="fa-brands fa-css3-alt" title="CSS3"></i>',85],
+                                ["JavaScript",'<i class="fa-brands fa-js" title="JavaScript"></i>',95],
+                                ["NodeJS",'<i class="fa-brands fa-node-js" title="NodeJS"></i>',70],
+                                ["MySQL",'<i class="fa-solid fa-database" title="MySQL"></i>',75],
+                                ["C#",'<i class="fa-brands fa-c" title="C#"></i><i class="fa-solid fa-hashtag" title="C#"></i>',67],
+                                ["C++",'<i class="fa-brands fa-c" title="C++"></i><i class="fa-solid fa-plus" title="C++"></i><i class="fa-solid fa-plus" title="C++"></i>',82],
+                                ["PHP",'<i class="fa-brands fa-php" title="PHP"></i>',78],
+                                ["Python",'<i class="fa-brands fa-python" title="Python"></i>',93],
+                                ["Testing",'<i class="fa-solid fa-code" title="Testing"></i>',69]];
 const skills = document.getElementsByClassName("skills")[0];
 const filas = Math.floor(abilitiesNamesPercentage.length / 5);
 var habilidades = "";
@@ -8,7 +18,7 @@ var cant = 0;
 for (let i = 0; i < filas; i++) {
     habilidades += '<div class="skill-bars">';
     for (let j = 0; j < 5; j++) {
-        habilidades += '<div class="bar"><div class="info"><span>' + abilitiesNamesPercentage[cant][0] + '</span></div><div class="progress-line"><span></span><div class="value">' + abilitiesNamesPercentage[cant][1] + '</div></div></div>';
+        habilidades += '<div class="bar"><div class="info">' + abilitiesNamesPercentage[cant][1] + '</div><div class="progress-line"><span></span><div class="value">' + abilitiesNamesPercentage[cant][2] + '</div></div></div>';
         cant++;
     }
     habilidades += '</div>';
